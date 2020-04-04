7bfdc7ee51dd47ec80362061ff34f669f2342b90
Tim Bijnen <>
2020-04-04 21:45:42 +0200

    Style joblink and add random values

--- app/models/job.js
+++ app/models/job.js
@@ -1,6 +1,19 @@
 import Model, { attr } from '@ember-data/model';
 
+const LOCATIONS = [ "Stockholm", "Krakau", "Eindhoven" ];
+
 export default class JobModel extends Model {
     @attr title;
     @attr body;
+    get location() {
+        const index = parseInt(this.id) % LOCATIONS.length;
+        return LOCATIONS[index];
+    }
+    get salary() {
+        const randomAddition = ( parseInt(this.id) % 20 ) * 100;
+        return 2000 + randomAddition;
+    }
+    get match() {
+        return parseInt(this.id) % 100;
+    }
 }
